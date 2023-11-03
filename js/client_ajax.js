20f60048b553be3bfe5f8581f63e4a23d8cfec89
Mr_cold <541892537@qq.com>
2023-11-03 23:18:27 +0800

    feat:新增client_ajax.js

---
+++ js/client_ajax.js
@@ -0,0 +1,12 @@
+(function () {
+    //定义客户端
+    var Client = {
+        isFirstConnect: true,
+        //当前用户
+        user: { uname: null },
+        //游戏信息
+        gameInfo: { user: null },
+    };
+
+    
+})();
