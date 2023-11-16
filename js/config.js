c4e12b0c7849d8b6045506de51e8d854d97e04a3
dreagonm <1748281467@qq.com>
2023-11-16 13:26:56 +0800

    添加加入onload事件的工具函数

--- js/config.js
+++ js/config.js
@@ -4,3 +4,16 @@ const SERVER_ADDR = "http://192.168.163.190:8000";
 // token 全局的登录token
 // room_id 加入成功后的房间号
 // username 登录成功的用户名
+
+function addLoadEvent(func){
+    var oldOnload = window.onload;
+    if(typeof oldOnload == "function"){
+        window.onload = function(){
+            oldOnload();
+            func();
+        };
+    }
+    else{
+        window.onload = func;
+    }
+}
